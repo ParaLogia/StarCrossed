c59ffdf6d358daf01a5c73e542e9b9daf6762744
Darren Ling <pantalaimonpan@gmail.com>
2018-05-02 14:55:36 -0400

    nvm i think we can use UTF-8

---
+++ DaPhiDa/src/ProfileServlet.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	                        throws ServletException, IOException {
+	    HttpSession session = request.getSession();
+	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	                        throws ServletException, IOException {
+		HttpSession session = request.getSession();
+	}
+}
