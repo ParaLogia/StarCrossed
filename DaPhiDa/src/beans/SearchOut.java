aade42c67c96d5f70e9772b68f2567cb8333641b
Darren Ling <pantalaimonpan@gmail.com>
2018-05-02 23:35:12 -0400

    Search Function should be working, height will be revisited

---
+++ DaPhiDa/src/beans/SearchOut.java
@@ -0,0 +1,77 @@
+package beans;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class SearchOut {
+	private String profileId;
+	private BigDecimal height;
+	private Integer weight;
+	private String hairColor;
+	private String city;
+	private String state;
+	private Timestamp dateOfLastAct;
+	private Integer rating;
+	
+	public SearchOut() {
+	}
+	public SearchOut(String profileId, BigDecimal height, Integer weight, String hairColor, String city,
+			String state, Timestamp dateOfLastAct, Integer rating) {
+		this.profileId = profileId;
+		this.height = height;
+		this.weight = weight;
+		this.hairColor = hairColor;
+		this.city = city;
+		this.state = state;
+		this.dateOfLastAct = dateOfLastAct;
+		this.rating = rating;
+	}
+	public String getProfileId() {
+		return profileId;
+	}
+	public void setProfileId(String profileId) {
+		this.profileId = profileId;
+	}
+	public BigDecimal getHeight() {
+		return height;
+	}
+	public void setHeight(BigDecimal height) {
+		this.height = height;
+	}
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	public String getHairColor() {
+		return hairColor;
+	}
+	public void setHairColor(String hairColor) {
+		this.hairColor = hairColor;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Timestamp getDateOfLastAct() {
+		return dateOfLastAct;
+	}
+	public void setDateOfLastAct(Timestamp dateOfLastAct) {
+		this.dateOfLastAct = dateOfLastAct;
+	}
+	public Integer getRating() {
+		return rating;
+	}
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+}
