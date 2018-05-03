9910610d3d43cff15405e7741d931a8914a3f7bd
Darren Ling <pantalaimonpan@gmail.com>
2018-05-02 20:03:29 -0400

    unused imports

--- DaPhiDa/src/LogoutServlet.java
+++ DaPhiDa/src/LogoutServlet.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.sql.*;
 
 
 @WebServlet(urlPatterns = { "/logout/submit" })
